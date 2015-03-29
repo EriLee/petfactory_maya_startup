d7a52eda2cd0fc4b721c8720f6d5464258440c67
Johan Borgström <johan@petfactory.se>
2015-03-29 15:13:54 +0200

    add custom menu label name

--- userSetup.py
+++ userSetup.py
@@ -19,8 +19,6 @@ parent_menu_name = 'Petfactory'
 
 
 def pet_init():
-    #script_dict = read_json(os.path.join(script_path, 'script_config.json'))
-    #build_petfactory_menu(script_dict)
     build_petfactory_menu()
 
 def read_json(file_path):
@@ -71,7 +69,7 @@ def open_dir(dir_name, absolute_path, *args):
             cmds.confirmDialog(t='Error', b=['OK'],m='Unable to open folder:\n{0}'.format(folder_path))
 
 
-def build_petfactory_menu(script_dict):
+def build_petfactory_menu():
 
     if cmds.menu('petfactory_menu', exists=1):
         cmds.deleteUI('petfactory_menu')
@@ -97,32 +95,54 @@ def build_petfactory_menu(script_dict):
     if script_dict:
 
         petfactory_custom_script_menu(script_dict, root_menu)
-        '''
-        category_dict = script_dict.get('scripts')
 
-        for category in sorted(category_dict):
+def petfactory_custom_script_menu(json_dict, parent_menu):
+
+    main_script_dict = json_dict.get('scripts')
     
-            script_menuitem = cmds.menuItem(category, p=root_menu, bld=1, sm=1, to=1, l=category)            
-            script_dict = category_dict[category]
-
-            for script in sorted(script_dict):
-
-                # build the string to be used for the menuItem button command.
-                # the script is imported when the menu item is pressed,
-                # the the cmd specified in the info_dict is called.
-
-                rel_path = script_dict[script].get('rel_path')
-                if rel_path:
-                    module = 'petfactory.{0}.{1}.{2}'.format(category, rel_path, script)
-                else:
-                    module = 'petfactory.{0}.{1}'.format(category, script)
-                
-                cmd = 'try:\n\texec "import {0}"\n\nexcept ImportError as e:\n\tprint "import error:", e\n'.format(module)
-                cmd += '\n{0}.{1}()'.format(module, script_dict[script].get('cmd'))
-                
-                cmds.menuItem(script, parent=script_menuitem, c=cmd, label=script)
-        '''
-
+    category_keys = main_script_dict.keys()
+    category_keys.sort()
+    
+    for category in category_keys:
+    
+        category_script_dict = main_script_dict[category]
+        category_script_keys = category_script_dict.keys()
+        category_script_keys.sort()
+    
+        script_menuitem = cmds.menuItem(category, p=parent_menu, bld=1, sm=1, to=1, l=category)  
+    
+        for category_script in category_script_keys:
+    
+            script_info_dict = category_script_dict[category_script]
+            
+            filename = script_info_dict.get('filename')
+            ext = script_info_dict.get('ext')
+            cmd = script_info_dict.get('cmd')
+    
+            if filename is None:
+                print('"filename" missing from {0}.{1}, continue...'.format(category, filename))
+                continue
+            if ext is None:
+                print('"ext" missing from {0}.{1}, continue...'.format(category, filename))
+                continue
+            elif cmd is None:
+                print('"cmd" missing from {0}.{1}, continue...'.format(category, filename))
+                continue
+            
+            # check if we have specified a sub dir (relative to the category dir)
+            rel_path = script_info_dict.get('rel_path')
+            
+            if rel_path:
+                module = 'petfactory.{0}.{1}.{2}'.format(category, rel_path, filename)
+            else:
+                module = 'petfactory.{0}.{1}'.format(category, filename)
+            
+            # construct the button press cmd
+            cmd_string = 'try:\n\texec "import {0}"\n\nexcept ImportError as e:\n\tprint "import error:", e\n'.format(module)
+            cmd_string += '\n{0}.{1}()'.format(module, cmd)
+            
+            cmds.menuItem(category_script, parent=script_menuitem, c=cmd_string, label=category_script)
+'''           
 def petfactory_custom_script_menu(script_dict, parent_menu):
 
     print('Called: petfactory_custom_script_menu')
@@ -150,5 +170,5 @@ def petfactory_custom_script_menu(script_dict, parent_menu):
             cmd += '\n{0}.{1}()'.format(module, script_dict[script].get('cmd'))
             
             cmds.menuItem(script, parent=script_menuitem, c=cmd, label=script)
-
+'''
 utils.executeDeferred('pet_init()')
